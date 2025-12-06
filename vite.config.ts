93943d6bdef7fbaedbc0b7d858c70f5fe187297b
GothiccMiaurwick <sebastiansoler3@gmail.com>
2025-12-05 20:41:34 -0500

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Calorie-tracker/', // Cambia esto por el nombre de tu repositorio en GitHub
 })
