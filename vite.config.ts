26102b0bfb34d79076c3a6a51fb45f5fdfecf7b2
GothiccMiaurwick <sebastiansoler3@gmail.com>
2025-12-05 20:46:02 -0500

    Configurar deploy automático con nombre de repositorio dinámico

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Calorie-tracker/', // Cambia esto por el nombre de tu repositorio en GitHub
+export default defineConfig(() => {
+  // Obtener el nombre del repositorio desde la variable de entorno
+  const repoName = process.env.VITE_REPO_NAME || 'Calorie-tracker'
+  
+  return {
+    plugins: [react()],
+    // Usa el nombre del repositorio desde la variable de entorno
+    base: `/${repoName}/`,
+  }
 })
